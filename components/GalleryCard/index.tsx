8c37c492d15f8943ec231bf92903c19498f0153b
Mohammed Ameen <ma6681221@gmail.com>
2024-12-01 19:59:28 +0530

    updated home page to retrieve images from cdn

--- components/GalleryCard/index.tsx
+++ components/GalleryCard/index.tsx
@@ -1,47 +1,101 @@
+'use client';
+
+import Loader from "@/components/Loader";
+import { client } from "@/sanity/lib/client";
+import { urlForImage } from "@/sanity/lib/image";
 import Link from 'next/link';
+import { useEffect, useState } from "react";
 import { FaArrowRight } from 'react-icons/fa';
 import styles from "./GalleryCard.module.css";
 
-export default function GalleryCard() {
+
+
+const Media = () => {
+    const [loading, setLoading] = useState(true);
+    const [galleryImages, setGalleryImages] = useState([]);
+
+    useEffect(() => {
+        const fetchImages = async () => {
+            setLoading(true);
+            try {
+                const images = await client.fetch(`*[_type=="gallery_images"]`);
+                console.log(images);
+                setGalleryImages(images);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchImages();
+    }, []);
+
     return (
         <div className={styles['container']}>
+
             <div className={styles['innerContainer']}>
                 <div className={styles['imageContainer']}>
-                    <div className={styles['imageRow']}>
-                        <img
-                            src={'/gallery_images/img-31.jpg'}
-                            className={styles['image']}
-                        />
+                    {loading || galleryImages.length === 0 ? (
+                        <div className={styles.loading}>
+                            <Loader loading={loading} />
+                        </div>
+                    ) : (
+                        <>
+                            <div className={styles['imageRow']}>
 
-                        <img
-                            src={'/gallery_images/img-32.jpg'}
-                            className={styles['image']}
-                        />
+                                {galleryImages.length > 30 && (
+                                    <img
+                                        src={urlForImage(galleryImages.find(item => item.title === "img-3").image)}
+                                        className={styles['image']}
+                                        alt="Gallery Image"
+                                    />
+                                )}
 
-                    </div>
-                    <div className={styles['imageRow']}>
-                        <img
-                            src={'/gallery_images/img-33.jpg'}
-                            className={styles['image']}
-                        />
+                                {galleryImages.length > 30 && (
+                                    <img
+                                        src={urlForImage(galleryImages.find(item => item.title === "img-32").image)}
+                                        className={styles['image']}
+                                        alt="Gallery Image"
+                                    />
+                                )}
 
-                        <Link className={styles['viewMore']} href={`/gallery`}>
-                            <div className={styles['viewMoreText']}>
-                                <span>VISIT</span>
-                                <span>FOSSMeeT&apos;24</span>
-                                <span style={{
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    flexDirection: 'row',
-                                    gap: '5px'
-                                }}>GALLERY
-                                    <FaArrowRight />
-                                </span>
+
+
                             </div>
-                        </Link>
-                    </div>
+                            <div className={styles['imageRow']}>
+
+
+                                {galleryImages.length > 30 && (
+                                    <img
+                                        src={urlForImage(galleryImages.find(item => item.title === "img-33").image)}
+                                        className={styles['image']}
+                                        alt="Gallery Image"
+                                    />
+                                )}
+
+                                <Link className={styles['viewMore']} href={`/gallery`}>
+                                    <div className={styles['viewMoreText']}>
+                                        <span>VISIT</span>
+                                        <span>FOSSMeeT&apos;24</span>
+                                        <span style={{
+                                            display: 'flex',
+                                            alignItems: 'center',
+                                            flexDirection: 'row',
+                                            gap: '5px'
+                                        }}>GALLERY
+                                            <FaArrowRight />
+                                        </span>
+                                    </div>
+                                </Link>
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
     )
-}
+};
+
+export default Media;
+
